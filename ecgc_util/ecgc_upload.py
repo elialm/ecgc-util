0e5faa5f8bd573209b5ca91323f37e01212b8e68
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2023-02-08 15:21:01 +0100

    Fixed error on no size given

--- ecgc_util/ecgc_upload.py
+++ ecgc_util/ecgc_upload.py
@@ -69,7 +69,7 @@ def main_cli():
     parser = ArgumentParser(prog='ecgc-upload', description='Utility for uploading code to ecgc project cartridge')
     parser.add_argument('serial_port', help='Serial port of the spi programmer')
     parser.add_argument('image_file', help='File to upload to the cartridge')
-    parser.add_argument('-s', '--size', default=0, help='Number for bytes to upload to the cartridge from the image file. If not given, will either upload entire file or fill the given target (if the file is equal of larger than the target)')
+    parser.add_argument('-s', '--size', default='0', type=str, help='Number for bytes to upload to the cartridge from the image file. If not given, will either upload entire file or fill the given target (if the file is equal of larger than the target)')
     parser.add_argument('-t', '--target', choices=('boot', 'dram', 'flash'), required=True, help='Destination target of the image upload')
     parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity of program output')
 
