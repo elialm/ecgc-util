799d503ef14748802a52132caa4dc9ef962d2e44
Elijah Almeida Coimbra <ealmeidacoimbra@gmail.com>
2024-02-13 16:24:39 +0100

    Fixed help information

--- src/ecgc_util/ecgc_debug.py
+++ src/ecgc_util/ecgc_debug.py
@@ -14,7 +14,51 @@ __LOG_LEVELS = (
     logging.DEBUG
 )
 
-__EPILOG = """
+__READ_EPILOG = """
+The read command reads from the specified address. An optional size parameter
+can be passed to specify how many bytes are to be read. By default, the size
+will be 1.
+
+When reading multiple bytes, the address will be incremented at each received
+byte. To disable this behaviour and fix the address at the given value, add
+the -f/--fixed flag.
+
+A couple of examples using the read command:
+    - Read a single byte from address $4000
+        > read $4000
+    - Read 16 bytes from addresses $0100-$01FF
+        > read $0100 16
+    - Read 256 bytes from address $A100
+        > read $A100 -f 256
+"""
+
+__WRITE_EPILOG = """
+The write command writes the given data to the specified address. When writing
+multiple bytes, the address will be incremented at each received byte. To
+disable this behaviour and fix the address at the given value, add the
+-f/--fixed flag.
+
+The data is given as a series of bytes separated by a space. Each value must
+fit within an 8-bit unsigned integer. To write unsigned data, add the
+-s/--signed flag.
+
+Data can also be given as a repeated pattern. When specifying the -r/--repeat
+flag, one can give a number TIMES which repeats the given data pattern TIMES
+times.
+
+A couple of examples using the write command:
+    - Write a single byte to address $4000
+        > write $4000 $FF
+    - Write the numbers 1-16 to addresses $0100-$01FF
+        > write $0100 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    - Write 256 bytes to address $A100
+        > write $A100 -f 256
+    - Write $00 to addresses $4000-$7FFF
+        > write $4000 -r $4000 $00
+
+"""
+
+__EPILOG = f"""
 The utility will open a command prompt for entering commands. Usable commands
 are documented below. These commands give the user the ability to peek and poke
 at registers inside the cartridge's memory space.
@@ -46,49 +90,13 @@ For example, the 16-bit address $0100 may also be written as $100.
 
 ## Read command
 
-The read command reads from the specified address. An optional size parameter
-can be passed to specify how many bytes are to be read. By default, the size
-will be 1.
-
-When reading multiple bytes, the address will be incremented at each received
-byte. To disable this behaviour and fix the address at the given value, add
-the -f/--fixed flag.
-
-### Read examples
-
-- Read a single byte from address $4000
-    > read $4000
-- Read 16 bytes from addresses $0100-$01FF
-    > read $0100 16
-- Read 256 bytes from address $A100
-    > read $A100 -f 256
+{__READ_EPILOG}
 
 ## Write command
 
-The write command writes the given data to the specified address. When writing
-multiple bytes, the address will be incremented at each received byte. To
-disable this behaviour and fix the address at the given value, add the
--f/--fixed flag.
-
-The data is given as a series of bytes separated by a space. Each value must
-fit within an 8-bit unsigned integer. To write unsigned data, add the
--s/--signed flag.
-
-Data can also be given as a repeated pattern. When specifying the -r/--repeat
-flag, one can give a number TIMES which repeats the given data pattern TIMES
-times.
-
-### Write examples
-
-- Write a single byte to address $4000
-    > write $4000 $FF
-- Write the numbers 1-16 to addresses $0100-$01FF
-    > write $0100 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-- Write 256 bytes to address $A100
-    > write $A100 -f 256
-- Write $00 to addresses $4000-$7FFF
-    > write $4000 -r $4000 $00
+{__WRITE_EPILOG}
 """
+
 
 class CommandError(Exception):
     pass
@@ -97,8 +105,9 @@ class SubArgumentParser(ArgumentParser):
     def error(self, message):
         raise ArgumentError(None, message)
 
+
 def construct_parser_read() -> SubArgumentParser:
-    parser = SubArgumentParser(prog='read', add_help=False, exit_on_error=False)
+    parser = SubArgumentParser(prog='read', epilog=__READ_EPILOG, formatter_class=RawTextHelpFormatter, add_help=False, exit_on_error=False)
     parser.add_argument('address', help='address to read from')
     parser.add_argument('-f', '--fixed', action='store_true', help='disable address increment on multi-byte operations')
     parser.add_argument('-s', '--size', default='1', help='number of bytes to read. defaults to 1')
@@ -106,14 +115,15 @@ def construct_parser_read() -> SubArgumentParser:
     return parser
 
 def construct_parser_write() -> SubArgumentParser:
-    parser = SubArgumentParser(prog='write', add_help=False, exit_on_error=False)
-    parser.add_argument('address')
-    parser.add_argument('-f', '--fixed', action='store_true')
-    parser.add_argument('-s', '--signed', action='store_true')
-    parser.add_argument('-r', '--repeat', )
-    parser.add_argument('data', nargs='+')
+    parser = SubArgumentParser(prog='write', epilog=__WRITE_EPILOG, formatter_class=RawTextHelpFormatter, add_help=False, exit_on_error=False)
+    parser.add_argument('address', help='address to write to')
+    parser.add_argument('data', nargs='+', help='data to be written')
+    parser.add_argument('-f', '--fixed', action='store_true', help='disable address increment on multi-byte operations')
+    parser.add_argument('-s', '--signed', action='store_true', help='switch from only accepting unsigned bytes, to only signed bytes')
+    parser.add_argument('-r', '--repeat', default='1', help='repeat the given data for the specified number of times')
 
     return parser
+
 
 class DebugShell(cmd.Cmd):
     intro = 'ecgc-debug 0.4a\ntype help or ? to list commands\n'
@@ -121,13 +131,12 @@ class DebugShell(cmd.Cmd):
     file = None
 
     def __init__(self, debugger: UartDebugger) -> None:
+        super().__init__()
         self.__debugger = debugger
         self.__parsers = {
             'read': construct_parser_read(),
             'write': construct_parser_write()
         }
-
-        super().__init__()
 
     def __print_error(self, error: Exception | str):
         if isinstance(error, Exception):
@@ -152,9 +161,16 @@ class DebugShell(cmd.Cmd):
     def help_read(self):
         self.__parsers['read'].print_help()
 
+    def do_write(self, arg):
+        args = self.__parse_args('write', arg)
+
+    def help_write(self):
+        self.__parsers['write'].print_help()
+
     def do_exit(self, arg):
-        """Exit the debugging utility"""
+        """Exits the debugging utility"""
         return True
+
 
 def main_cli():
     parser = ArgumentParser(prog='ecgc-debug', description='Utility for peeking and poking cartridge registers', epilog=__EPILOG, formatter_class=RawTextHelpFormatter)
